ab8d1db3f689d8651477d7f210f9fe5601c0af4e
Paweł Reliński <pawelrelinski@gmail.com>
2021-01-21 16:07:04 +0100

    Add display categories to category-list.component

--- libs/category/src/lib/category-list/category-list.component.ts
+++ libs/category/src/lib/category-list/category-list.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IHeaderData {
+  title: string;
+}
+
+interface ICategory {
+  name: string;
+  subcategories: Array<string>;
+}
+
 @Component({
   selector: 'customify-category-list',
   templateUrl: './category-list.component.html',
@@ -7,12 +16,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  public pageIntroHeaderData;
+  public pageIntroHeaderData: IHeaderData;
+  public categories: Array<ICategory> = [];
+  public currentDisplayCategory: ICategory;
+  public categoryIsChoose = false;
+  public btnCategoryNameIsActive = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initHeader();
+    this.initCategories();
+  }
+
+  public showCategoryElements(name: string): void {
+    this.categoryIsChoose = true;
+    this.currentDisplayCategory = this.getCategoryDataByName(name);
+  }
+
+  private initHeader(): void {
     this.pageIntroHeaderData = { title: 'Categories' };
+  }
+
+  private initCategories(): void {
+    this.categories.push(
+      { name: 'women', subcategories: [ 'shirts', 'dress', 'boots' ] },
+      { name: 'men', subcategories: [ 't-shirts', 'jeans', 'boots' ] },
+      { name: 'kids', subcategories: [ 'bodies', 'heat', 'boots' ] }
+    );
+  }
+
+  private getCategoryDataByName(name: string): ICategory {
+    return this.categories.find(category => category.name === name);
   }
 
 }
