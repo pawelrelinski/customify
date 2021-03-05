9812cbc740dbc05718e17d371c955e8a9450892e
Paweł Reliński <pawelrelinski@gmail.com>
2021-03-05 14:54:20 +0100

    Add fade in/out animation for changing widget

--- apps/client/src/app/modules/home/components/widget-spinner/widget-spinner.component.ts
+++ apps/client/src/app/modules/home/components/widget-spinner/widget-spinner.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IWidget } from '../widget/widget.component';
 
+export const INTERVAL_TIME_IN_MS = 5_000;
+
 @Component({
   selector: 'customify-widget-spinner',
   templateUrl: './widget-spinner.component.html',
@@ -9,51 +11,37 @@ import { IWidget } from '../widget/widget.component';
 export class WidgetSpinnerComponent implements OnInit, OnDestroy {
 
   public widgets: Array<IWidget> = [
-    {
-      img: './assets/mockup_products/apple-watch.png',
-      mainText: 'Apple Watch',
-      secondText: 'USD 279'
-    },
-    {
-      img: './assets/mockup_products/ipad.png',
-      mainText: 'iPad Pro',
-      secondText: 'USD 1029'
-    },
-    {
-      img: './assets/mockup_products/iphone-12.png',
-      mainText: 'iPhone 12 Pro',
-      secondText: 'USD 1199'
-    }
+    { img: './assets/mockup_products/apple-watch.png', mainText: 'Apple Watch', secondText: 'USD 279' },
+    { img: './assets/mockup_products/ipad.png', mainText: 'iPad Pro', secondText: 'USD 1029' },
+    { img: './assets/mockup_products/iphone-12.png', mainText: 'iPhone 12 Pro', secondText: 'USD 1199' }
   ];
 
   public indexOfCurrentWidget = 0;
 
-  private interval: number;
+  private intervalId: number;
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.setIntervalForChangeCurrentWidget();
+  async ngOnInit(): Promise<void> {
+    await this.setIntervalForChangeCurrentWidget();
   }
 
   ngOnDestroy(): void {
-    clearInterval(this.interval);
+    clearInterval(this.intervalId);
   }
 
-  public changeCurrentWidget(index: number) {
+  public changeCurrentWidget(index: number): void {
     this.indexOfCurrentWidget = index;
   }
 
-  private setIntervalForChangeCurrentWidget() {
-    const TIME_IN_MS = 5_000;
-
-    this.interval = setInterval(() => {
+  private async setIntervalForChangeCurrentWidget(): Promise<void> {
+    this.intervalId = setInterval(() => {
       if (this.indexOfCurrentWidget === this.widgets.length - 1) {
         this.indexOfCurrentWidget = 0;
       } else {
         this.indexOfCurrentWidget += 1;
       }
-    }, TIME_IN_MS);
+    }, INTERVAL_TIME_IN_MS);
   }
 
 }
--- apps/client/src/app/modules/home/components/widget/widget.component.ts
+++ apps/client/src/app/modules/home/components/widget/widget.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { INTERVAL_TIME_IN_MS } from '../widget-spinner/widget-spinner.component';
 
 export interface IWidget {
   img: string;
@@ -6,15 +8,68 @@ export interface IWidget {
   secondText: string;
 }
 
+enum changeWidgetStates {
+  OUT = 'out',
+  IN = 'in'
+}
+
 @Component({
   selector: 'customify-widget',
+  animations: [
+    trigger('changeWidget', [
+      state(changeWidgetStates.IN, style({
+        opacity: 1
+      })),
+      state(changeWidgetStates.OUT, style({
+        opacity: 0.1
+      })),
+      transition('in=>out', animate('.5s')),
+      transition('out=>in', animate('.5s')),
+    ])
+  ],
   templateUrl: './widget.component.html',
   styleUrls: ['./widget.component.scss']
 })
-export class WidgetComponent {
+export class WidgetComponent implements OnInit, OnDestroy {
 
   @Input() widget: IWidget;
 
+  public currentStateAnimation: changeWidgetStates;
+
+  private intervalId: number;
+
   constructor() { }
+
+  async ngOnInit(): Promise<void> {
+    await this.setIntervalForChangeCurrentWidget();
+    this.setCurrentStateAnimationToIn();
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  private async setIntervalForChangeCurrentWidget(): Promise<void> {
+    const WAIT_TIME_IN_MS = INTERVAL_TIME_IN_MS - 500;
+
+    this.wait(WAIT_TIME_IN_MS).then(() => this.setCurrentStateAnimationToOut());
+
+    setInterval(() => {
+      this.wait(WAIT_TIME_IN_MS).then(() => this.setCurrentStateAnimationToOut());
+      this.setCurrentStateAnimationToIn();
+    }, INTERVAL_TIME_IN_MS);
+  }
+
+  private wait(time: number): Promise<unknown> {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+
+  private setCurrentStateAnimationToOut(): void {
+    this.currentStateAnimation = changeWidgetStates.OUT;
+  }
+
+  private setCurrentStateAnimationToIn(): void {
+    this.currentStateAnimation = changeWidgetStates.IN;
+  }
 
 }
